7bf5b531859af0c6122beb9ef583abdfdd743c0a
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-23 21:37:16 +0330

    working on app layout

--- src/layOuts/admin/Admin.jsx
+++ src/layOuts/admin/Admin.jsx
@@ -1,3 +1,10 @@
+import styles from "./Admin.module.scss";
+
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div className={styles.king}>
+      <div className={styles.sideBarContainer}></div>
+      <div className={styles.mainContentContainer}></div>
+    </div>
+  );
 }

2d18c20709257fd290f89b9764a90eaeade9342d
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-12-12 00:18:04 +0330

    now we have date on header

--- src/layOuts/admin/components/header/components/leftHeaderContainer/components/headerDate/HeaderDate.jsx
+++ src/layOuts/admin/components/header/components/leftHeaderContainer/components/headerDate/HeaderDate.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { days, months } from "./dateInfo";
 import styles from "./HeaderDate.module.scss";
 
 export default function HeaderDate() {
-  return <div className={styles.king}>Monday, December 9</div>;
+  const [date, setDate] = useState(new Date());
+  const month = months[date.getMonth()];
+  const day = days[date.getDay()];
+  const dayNum = date.getDay();
+  const sec = date.getSeconds();
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
+  return (
+    <div className={styles.king}>
+      {day}, {month} {dayNum}
+    </div>
+  );
 }

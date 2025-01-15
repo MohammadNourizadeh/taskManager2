965a82604b7e6d187bf6d6b66d3a7d27167f0a2a
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-15 16:32:15 +0330

    now tou can see checked tasks on checked tasks page

--- src/pages/checkedTasksPage/CheckedTasksPage.jsx
+++ src/pages/checkedTasksPage/CheckedTasksPage.jsx
@@ -1,3 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import Task from "../../components/task/Task";
+import MainContext from "../../contexts/MainContext";
+import styles from "./CheckedTasksPage.module.scss";
+import EmptyPageText from "../../components/emptyPageText/EmptyPageText";
+
 export default function CheckedTasksPage() {
-  return <div>CheckedTasksPage</div>;
+  // context
+  const { tasks, setTasks } = useContext(MainContext);
+
+  // state
+  const [checkedTasksList, setCheckedTasksList] = useState([]);
+
+  // side effect
+  useEffect(() => {
+    fetch("http://localhost:8000/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        const checkedTasks = data.filter((item) => item.done === true);
+        setCheckedTasksList(checkedTasks);
+      });
+  }, [tasks]);
+
+  return (
+    <>
+      {checkedTasksList.length > 0 ? (
+        <div className={styles.king}>
+          {checkedTasksList.map((task) => (
+            <Task
+              task={task}
+              tasks={tasks}
+              onSetNewList={(val) => {
+                setTasks(val);
+              }}
+              key={task.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <EmptyPageText text={"checked task"} />
+      )}
+    </>
+  );
 }

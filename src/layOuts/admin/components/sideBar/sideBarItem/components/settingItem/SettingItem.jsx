3d20f27d0a3c0a8b6923ff0e3ae3ec5baa2c6909
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-18 13:35:53 +0330

    working on setting options

--- src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/SettingItem.jsx
+++ src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/SettingItem.jsx
@@ -1,13 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./SettingItem.module.scss";
+import { faMoon, faPerson, faUserCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function SettingItem({ iconName, itemName }) {
   return (
-    <li className={styles.king}>
-      <span>
-        <FontAwesomeIcon icon={iconName} />
-      </span>
-      {itemName}
-    </li>
+    <>
+      <li className={styles.king}>
+        <span>
+          <FontAwesomeIcon icon={iconName} />
+        </span>
+        {itemName}
+      </li>
+      <li className={styles.settingDropDown}>
+        <form className={styles.settingForm}>
+          <ul className={styles.settingOptionsList}>
+            <li>
+              <div className={styles.dropDownIconContainer}>
+                <FontAwesomeIcon icon={faUserCog} />
+              </div>
+              <div className={styles.dropDownItemNameContainer}>username</div>
+              <div className={styles.dropDownBtnContainer}>
+                <button>edit</button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.dropDownIconContainer}>
+                <FontAwesomeIcon icon={faMoon} />
+              </div>
+              <div className={styles.dropDownItemNameContainer}>theme</div>
+              <div className={styles.dropDownBtnContainer}>
+                <select name="theme" id="darkAndLightMode">
+                  <option value="dark">dark</option>
+                  <option value="light">light</option>
+                </select>
+              </div>
+            </li>
+          </ul>
+        </form>
+      </li>
+    </>
   );
 }

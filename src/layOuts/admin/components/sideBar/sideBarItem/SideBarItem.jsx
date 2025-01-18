4d1d704c832aecbfbd040f43615db0d14b72e4fa
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-18 13:43:18 +0330

    working on setting options

--- src/layOuts/admin/components/sideBar/sideBarItem/SideBarItem.jsx
+++ src/layOuts/admin/components/sideBar/sideBarItem/SideBarItem.jsx
@@ -33,6 +33,13 @@ export default function SideBarItem({
       }}
     />
   ) : (
-    <SettingItem iconName={iconName} itemName={itemName} />
+    <SettingItem
+      iconName={iconName}
+      itemName={itemName}
+      selectedItem={selectedItem}
+      onSelect={(itemName) => {
+        setSelectedItem(itemName);
+      }}
+    />
   );
 }
--- src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/SettingItem.jsx
+++ src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/SettingItem.jsx
@@ -2,42 +2,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./SettingItem.module.scss";
 import { faMoon, faPerson, faUserCog } from "@fortawesome/free-solid-svg-icons";
 
-export default function SettingItem({ iconName, itemName }) {
+export default function SettingItem({
+  iconName,
+  itemName,
+  selectedItem,
+  onSelect,
+}) {
   return (
     <>
-      <li className={styles.king}>
+      <li
+        className={selectedItem === itemName ? styles.selected : styles.unSelected}
+        onClick={() => {
+          onSelect(itemName);
+        }}
+      >
         <span>
           <FontAwesomeIcon icon={iconName} />
         </span>
         {itemName}
       </li>
-      <li className={styles.settingDropDown}>
-        <form className={styles.settingForm}>
-          <ul className={styles.settingOptionsList}>
-            <li>
-              <div className={styles.dropDownIconContainer}>
-                <FontAwesomeIcon icon={faUserCog} />
-              </div>
-              <div className={styles.dropDownItemNameContainer}>username</div>
-              <div className={styles.dropDownBtnContainer}>
-                <button>edit</button>
-              </div>
-            </li>
-            <li>
-              <div className={styles.dropDownIconContainer}>
-                <FontAwesomeIcon icon={faMoon} />
-              </div>
-              <div className={styles.dropDownItemNameContainer}>theme</div>
-              <div className={styles.dropDownBtnContainer}>
-                <select name="theme" id="darkAndLightMode">
-                  <option value="dark">dark</option>
-                  <option value="light">light</option>
-                </select>
-              </div>
-            </li>
-          </ul>
-        </form>
-      </li>
+      {selectedItem === itemName && (
+        <li className={styles.settingDropDown}>
+          <form className={styles.settingForm}>
+            <ul className={styles.settingOptionsList}>
+              <li>
+                <div className={styles.dropDownIconContainer}>
+                  <FontAwesomeIcon icon={faUserCog} />
+                </div>
+                <div className={styles.dropDownItemNameContainer}>username</div>
+                <div className={styles.dropDownBtnContainer}>
+                  <button>edit</button>
+                </div>
+              </li>
+              <li>
+                <div className={styles.dropDownIconContainer}>
+                  <FontAwesomeIcon icon={faMoon} />
+                </div>
+                <div className={styles.dropDownItemNameContainer}>theme</div>
+                <div className={styles.dropDownBtnContainer}>
+                  <select name="theme" id="darkAndLightMode">
+                    <option value="dark">dark</option>
+                    <option value="light">light</option>
+                  </select>
+                </div>
+              </li>
+            </ul>
+          </form>
+        </li>
+      )}
     </>
   );
 }

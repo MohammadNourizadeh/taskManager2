932b6c6604a25f30af98f98cd9bf367215acca1f
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-18 16:36:10 +0330

    now you can see username

--- src/layOuts/admin/components/sideBar/SideBar.jsx
+++ src/layOuts/admin/components/sideBar/SideBar.jsx
@@ -1,12 +1,26 @@
+import { useContext, useEffect } from "react";
 import styles from "./SideBar.module.scss";
 import SideBarItem from "./sideBarItem/SideBarItem";
 import { sideBarItems } from "./sideBarItems";
 import SideBarUsernameItem from "./sideBarUsernameItem/SideBarUsernameItem";
+import MainContext from "../../../../contexts/MainContext";
 
 export default function SideBar() {
+  // context
+  const { appSetting, setAppSetting } = useContext(MainContext);
+
+  // side effect
+  useEffect(() => {
+    fetch("http://localhost:8000/setting")
+      .then((res) => res.json())
+      .then((data) => {
+        setAppSetting(data);
+      });
+  }, []);
+
   return (
     <ul className={styles.king}>
-      <SideBarUsernameItem />
+      <SideBarUsernameItem username={appSetting.username} />
       <hr />
       {sideBarItems.map((sideBarItem, index) => (
         <SideBarItem
--- src/layOuts/admin/components/sideBar/sideBarUsernameItem/SideBarUsernameItem.jsx
+++ src/layOuts/admin/components/sideBar/sideBarUsernameItem/SideBarUsernameItem.jsx
@@ -2,13 +2,13 @@ import { faCircleUser, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./SideBarUsernameItem.module.scss";
 
-export default function SideBarUsernameItem() {
+export default function SideBarUsernameItem({ username }) {
   return (
     <li className={styles.king}>
       <span>
         <FontAwesomeIcon icon={faCircleUser} />
       </span>
-      <div>user</div>
+      <div>{username}</div>
     </li>
   );
 }

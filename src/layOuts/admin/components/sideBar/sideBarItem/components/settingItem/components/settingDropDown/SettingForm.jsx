f2f24e9da8af7756032c60a631efba78cf0c3e1f
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-19 16:49:01 +0330

    working on setting form but save and discard btn are not working

--- src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/components/settingDropDown/SettingForm.jsx
+++ src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/components/settingDropDown/SettingForm.jsx
@@ -1,43 +1,14 @@
-import { faMoon, faUserCog } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext, useState } from "react";
 import MainContext from "../../../../../../../../../contexts/MainContext";
 import styles from "./SettingForm.module.scss";
+import SettingOptions from "./components/settingOptions/SettingOptions";
 
 export default function SettingForm() {
   // context
   const { appSetting } = useContext(MainContext);
 
   // state
-  const [isEditingUsername, setIsEditingUsername] = useState(false);
   const [isFormSaveBtnOpen, setIsFormSaveBtnOpen] = useState(false);
-  const [inputText, setInputText] = useState(appSetting.username);
-  const [selectVal, setSelectVal] = useState(appSetting.theme);
-
-  // func
-  const handleEdit = (  ) => {
-    if (inputText !== appSetting.username) {
-      setIsFormSaveBtnOpen(true);
-    } else if (
-      inputText === appSetting.username &&
-      selectVal === appSetting.theme
-    ) {
-      setIsFormSaveBtnOpen(false);
-    }
-    setIsEditingUsername((prev) => !prev);
-  };
-
-  const handleChangeAppTheme = (e) => {
-    setSelectVal(e.target.value);
-    if (selectVal === appSetting.theme) {
-      setIsFormSaveBtnOpen(true);
-    } else if (
-      inputText === appSetting.username &&
-      selectVal !== appSetting.theme
-    ) {
-      setIsFormSaveBtnOpen(false);
-    }
-  };
 
   const handleSaveChanges = () => {
     fetch("http://localhost:8000/setting", {
@@ -57,47 +28,11 @@ export default function SettingForm() {
   return (
     <div className={styles.king}>
       <ul className={styles.settingOptionsList}>
-        <li>
-          <div className={styles.dropDownIconContainer}>
-            <FontAwesomeIcon icon={faUserCog} />
-          </div>
-          <div className={styles.dropDownUsernameContainer}>
-            {isEditingUsername ? (
-              <input
-                type="text"
-                name="username"
-                value={inputText}
-                onChange={(e) => {
-                  setInputText(e.target.value);
-                }}
-              />
-            ) : (
-              <span>{inputText}</span>
-            )}
-          </div>
-          <div className={styles.dropDownBtnContainer}>
-            <button onClick={handleEdit}>
-              {isEditingUsername ? "done" : "edit"}
-            </button>
-          </div>
-        </li>
-        <li>
-          <div className={styles.dropDownIconContainer}>
-            <FontAwesomeIcon icon={faMoon} />
-          </div>
-          <div className={styles.dropDownItemNameContainer}>theme</div>
-          <div className={styles.dropDownBtnContainer}>
-            <select
-              name="theme"
-              id="darkAndLightMode"
-              value={selectVal}
-              onChange={handleChangeAppTheme}
-            >
-              <option value="dark">dark</option>
-              <option value="light">light</option>
-            </select>
-          </div>
-        </li>
+        <SettingOptions
+          onOpenSaveBtn={(val) => {
+            setIsFormSaveBtnOpen(val);
+          }}
+        />
       </ul>
       {isFormSaveBtnOpen && (
         <div className={styles.settingFormBtns}>

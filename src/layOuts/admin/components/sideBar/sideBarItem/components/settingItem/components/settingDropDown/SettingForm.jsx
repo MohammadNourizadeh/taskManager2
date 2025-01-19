173436fc29ded1bb5d8bf7e15e9c1a0fa8d5035e
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-01-19 14:02:46 +0330

    working on setting form

--- src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/components/settingDropDown/SettingForm.jsx
+++ src/layOuts/admin/components/sideBar/sideBarItem/components/settingItem/components/settingDropDown/SettingForm.jsx
@@ -11,9 +11,51 @@ export default function SettingForm() {
   // state
   const [isEditingUsername, setIsEditingUsername] = useState(false);
   const [isFormSaveBtnOpen, setIsFormSaveBtnOpen] = useState(false);
+  const [inputText, setInputText] = useState(appSetting.username);
+  const [selectVal, setSelectVal] = useState(appSetting.theme);
+
+  // func
+  const handleEdit = (  ) => {
+    if (inputText !== appSetting.username) {
+      setIsFormSaveBtnOpen(true);
+    } else if (
+      inputText === appSetting.username &&
+      selectVal === appSetting.theme
+    ) {
+      setIsFormSaveBtnOpen(false);
+    }
+    setIsEditingUsername((prev) => !prev);
+  };
+
+  const handleChangeAppTheme = (e) => {
+    setSelectVal(e.target.value);
+    if (selectVal === appSetting.theme) {
+      setIsFormSaveBtnOpen(true);
+    } else if (
+      inputText === appSetting.username &&
+      selectVal !== appSetting.theme
+    ) {
+      setIsFormSaveBtnOpen(false);
+    }
+  };
+
+  const handleSaveChanges = () => {
+    fetch("http://localhost:8000/setting", {
+      method: "PATCH",
+      body: JSON.stringify({ username: inputText, theme: selectVal }),
+    });
+    setIsFormSaveBtnOpen(false);
+  };
+
+  const handleDiscardChanges = () => {
+    setInputText(appSetting.username);
+    setSelectVal(appSetting.theme);
+    setIsEditingUsername(false);
+    setIsFormSaveBtnOpen(false);
+  };
 
   return (
-    <form className={styles.king}>
+    <div className={styles.king}>
       <ul className={styles.settingOptionsList}>
         <li>
           <div className={styles.dropDownIconContainer}>
@@ -21,9 +63,16 @@ export default function SettingForm() {
           </div>
           <div className={styles.dropDownUsernameContainer}>
             {isEditingUsername ? (
-              <input type="text" placeholder="username..." name="username" />
+              <input
+                type="text"
+                name="username"
+                value={inputText}
+                onChange={(e) => {
+                  setInputText(e.target.value);
+                }}
+              />
             ) : (
-              <span>{appSetting.username}</span>
+              <span>{inputText}</span>
             )}
           </div>
           <div className={styles.dropDownBtnContainer}>
@@ -41,6 +90,7 @@ export default function SettingForm() {
             <select
               name="theme"
               id="darkAndLightMode"
+              value={selectVal}
               onChange={handleChangeAppTheme}
             >
               <option value="dark">dark</option>
@@ -51,10 +101,14 @@ export default function SettingForm() {
       </ul>
       {isFormSaveBtnOpen && (
         <div className={styles.settingFormBtns}>
-          <button className={styles.saveBtn}>save</button>
-          <button className={styles.resetBtn}>reset</button>
+          <button className={styles.saveBtn} onClick={handleSaveChanges}>
+            save
+          </button>
+          <button className={styles.resetBtn} onClick={handleDiscardChanges}>
+            discard
+          </button>
         </div>
       )}
-    </form>
+    </div>
   );
 }
